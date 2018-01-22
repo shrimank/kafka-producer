0c02b4cbfa9eb5dc1fc4d399d14f0680c2a13f17
shrimank <shrimankumbar@gmail.com>
2018-01-22 17:51:19 +0530

    Producer and consumer comment added

--- src/main/java/com/evolvus/kafka/kafkaproducer/KafkaProducerRouteBuilder.java
+++ src/main/java/com/evolvus/kafka/kafkaproducer/KafkaProducerRouteBuilder.java
@@ -8,9 +8,11 @@ public class KafkaProducerRouteBuilder extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
+		//Producer
 		from("file:///home/user/Desktop/ITems/Training-23-Jan-2018/kafka-producer/source?antInclude=*.xml")
 		.to("kafka:MyFirstTopic1?brokers=localhost:9092&groupId=testgroup");
 
+		//Consumer
 		from("kafka:MyFirstTopic1?brokers=localhost:9092&groupId=testgroup")
 		.to("file:///home/user/Desktop/ITems/Training-23-Jan-2018/kafka-producer/destination");
 	}
